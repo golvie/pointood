6252c6be3c580ef6eae3208e581f159828e831ee
judin.jaroslav <judin.jaroslav@3f70cdb2-1ca0-11df-8975-b7b9183c216b>
2010-02-18 15:21:23 +0000

    add

---
+++ trunk/ood_har_02/src/Controller.java
@@ -0,0 +1,44 @@
+
+/**
+ * @author J
+ *
+ */
+public class Controller {
+
+	Route route = new Route();
+	
+	public void add(double x, double y){
+		route.add(new Point(x,y));
+	}
+	
+	public void remove(double x, double y){
+		if(route.remove(new Point(x,y)))
+			System.out.println("The point: "+x+"; "+y+"; is removed");
+		else
+			System.out.println("The point: "+x+"; "+y+" cannot be removed. It isn't in the Route.");
+	}
+	
+	public double distance(){
+		return route.length();
+	}
+	public void print(){
+		route.print();
+	}
+	
+	/*
+	public static void main(String[] args) {
+	
+		Controller c = new Controller();
+		c.add(10, 20);
+		c.add(17, 0);
+		c.add(17, 10);
+		
+		c.print();
+		
+		c.remove(17.00,0.00);
+		c.remove(17.00,1.00);
+		c.print();
+		System.out.println("Distance: " +c.distance());
+	}
+	*/
+}
---
+++ trunk/ood_har_02/src/input.java
@@ -0,0 +1,55 @@
+
+import java.io.*;
+
+/**
+ * @author J
+ *
+ */
+public class input {
+
+	
+	
+	public static double getCoord(String c) {
+		double coord;
+		String str="";
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("Enter coordinate "+c+": ");
+		do {
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+			try {
+				coord = Double.parseDouble(str);
+			} catch (NumberFormatException e) {
+				System.out.println("Error value. Enter "+c+" once again:");
+				coord = Double.NEGATIVE_INFINITY;
+			}
+		} while (coord  == Double.NEGATIVE_INFINITY);
+		
+		return coord;
+	}
+
+	public static String getFlag() {
+		String str="";
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("Enter 's'-stop, 'd'-route distance, 'a'-add or  'r'-remove:");
+		do {
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+			if(!str.equalsIgnoreCase("a") && !str.equalsIgnoreCase("r")
+					&& !str.equalsIgnoreCase("s")&& !str.equalsIgnoreCase("d")) {
+				System.out.println("Only 'S', 'A', 'D' or 'R':");
+				str="";
+			}
+		} while (str.equals(""));
+		return str;
+	}
+	
+}
