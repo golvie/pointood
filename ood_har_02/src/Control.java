f37f8a53e5f30ce971ed7bef7c7d5dbab95f65de
judin.jaroslav <judin.jaroslav@3f70cdb2-1ca0-11df-8975-b7b9183c216b>
2010-02-18 20:18:32 +0000

    add

---
+++ ood_har_02/src/Control.java
@@ -0,0 +1,13 @@
+
+
+public interface Control{
+	
+	public void add(double x, double y);
+	
+	public void remove(double x, double y);
+	
+	public double distance();
+
+}
+
+
--- ood_har_02/src/Controller.java
+++ ood_har_02/src/Controller.java
@@ -3,14 +3,16 @@
  * @author J
  *
  */
-public class Controller {
+public class Controller implements Control {
 
 	Route route = new Route();
 	
+	@Override
 	public void add(double x, double y){
 		route.add(new Point(x,y));
 	}
 	
+	@Override
 	public void remove(double x, double y){
 		if(route.remove(new Point(x,y)))
 			System.out.println("The point: "+x+"; "+y+"; is removed");
@@ -18,12 +20,14 @@ public class Controller {
 			System.out.println("The point: "+x+"; "+y+" cannot be removed. It isn't in the Route.");
 	}
 	
+	@Override
 	public double distance(){
 		return route.length();
 	}
 	public void print(){
 		route.print();
 	}
+
 	
 	/*
 	public static void main(String[] args) {
