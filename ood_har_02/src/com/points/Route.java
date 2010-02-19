02c9c9fddeb2a8028b8f20427dcf0ba884756b90
judin.jaroslav <judin.jaroslav@3f70cdb2-1ca0-11df-8975-b7b9183c216b>
2010-02-19 22:22:37 +0000

    JU

--- ood_har_02/src/com/points/Route.java
+++ ood_har_02/src/com/points/Route.java
@@ -12,19 +12,28 @@ public class Route {
 
 	private ArrayList<Point> points = new ArrayList<Point>();
 	
-	public void print(){
-		System.out.print("[");
-		for(Point p : points)
-			if(getIdx(p)==(points.size()-1))
-				System.out.print("("+p+")");
-			else
-				System.out.print("("+p+"),");
-		System.out.print("] Distance: "+this.length()+"\n");
-	}
 	
 	public void add(Point p){
 		points.add(p);
 	}
+	
+	public boolean remove(Point p){
+		if(getIdx(p)>-1){
+			points.remove(this.getIdx(p));
+			return true;
+		}
+		else
+			return false;	
+	}
+	
+	public double distance(){
+		double length=0;
+		for(int i=1; i<points.size(); i++){
+			length += points.get(i-1).distance(points.get(i));
+		}
+		return length;
+	}
+
 	
 	private int getIdx(Point p){
 		int idx=-1;
@@ -36,24 +45,15 @@ public class Route {
 		return idx;
 	}
 	
-	public boolean remove(Point p){
-		//if(p.eqCrd(points.get(count)) || p.eqCrd(points.get(0))){
-		if(getIdx(p)>-1){
-			points.remove(this.getIdx(p));
-			return true;
-		}
-		else
-			return false;
-		
-	}
 	
-	public double length(){
-		double length=0;
-		for(int i=1; i<points.size(); i++){
-			length += points.get(i-1).distance(points.get(i));
-		}
-		return length;
+	public void print(){
+		System.out.print("[");
+		for(Point p : points)
+			if(getIdx(p)==(points.size()-1))
+				System.out.print("("+p+")");
+			else
+				System.out.print("("+p+"),");
+		System.out.print("] Distance: "+this.distance()+"\n");
 	}
-
 	
 }
